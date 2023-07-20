a322a4f15cf16a74c5d4f745ae96b6fa2fa526b0
s4intify <s4intify@proton.me>
2023-07-20 23:39:18 +0200

    Creating the GLFW window

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,71 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+// Constants
+
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+const char *WINDOW_TITLE = "GLFW";
 
 int main()
 {
+  // Initializing GLFW
+
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Creating the Window
+
+  GLFWwindow *window = glfwCreateWindow(
+      WINDOW_WIDTH,
+      WINDOW_HEIGHT,
+      WINDOW_TITLE,
+      NULL,
+      NULL);
+  if (window == NULL)
+  {
+    printf("Failed to initialize the GLFW window!\n");
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // Initializing GLEW
+
+  GLenum err = glewInit();
+  if (GLEW_OK != err)
+  {
+    printf("Failed to initialize the GLFW window!\n");
+    printf("Error: %s\n", glewGetErrorString(err));
+    glfwTerminate();
+    return 1;
+  }
+  printf("GLEW: %s\n", glewGetString(GLEW_VERSION));
+
+  // Clear Color
+
+  GLclampf red = 0.0f;
+  GLclampf blue = 0.3f;
+  GLclampf green = 0.3f;
+  GLclampf alpha = 1.0f;
+  glClearColor(red, blue, green, alpha);
+
+  // Main Loop
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+  }
+
+  // Terminating the Program
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return EXIT_SUCCESS;
 }
